594c3ab20fed6d67d2d701046a8a928f244e3919
Milene1Falcon <luizgabrielalvesfernandes67@gmail.com>
2025-05-29 21:19:37 -0300

    Atualização Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,5 +17,8 @@ const relogio = setInterval(function time() {
     horas.textContent = hr + '';
     minutos.textContent = min + '';
     segundos.textContent = s;
+
 })
+
+const currentduration = document.querySelectorAll ('progressbar')
  
